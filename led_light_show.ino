938f8689d5a3079cce51dd522b7280abdafcf653
Vetaz <47232579+Vetaz@users.noreply.github.com>
2020-10-24 09:22:43 -0600

    added Sam's code

--- led_light_show.ino
+++ led_light_show.ino
@@ -1,188 +1,151 @@
-#include <FastLED.h>
+#include "FastLED.h"
 
-#define LED_PIN     5
-#define NUM_LEDS    50
-#define BRIGHTNESS  64
-#define LED_TYPE    WS2811
-#define COLOR_ORDER GRB
-CRGB leds[NUM_LEDS];
+#define NUM_LEDS 300        // How many leds in strip
+#define updateLEDS 8        // How many do you want to update every millisecond
+#define COLOR_SHIFT 180000  // Time for colours to shift to a new spectrum (in ms)
+CRGB leds[NUM_LEDS];        // Define array of leds
 
-#define UPDATES_PER_SECOND 100
+// Define the digital I/O PINS..
+#define DATA_PIN 6          // led data transfer
+#define PITCH_PIN 0         // pitch input from frequency to voltage converter
+#define BRIGHT_PIN 4        // brightness input from amplified audio signal
 
-// This example shows several ways to set up and use 'palettes' of colors
-// with FastLED.
-//
-// These compact palettes provide an easy way to re-colorize your
-// animation on the fly, quickly, easily, and with low overhead.
-//
-// USING palettes is MUCH simpler in practice than in theory, so first just
-// run this sketch, and watch the pretty lights as you then read through
-// the code.  Although this sketch has eight (or more) different color schemes,
-// the entire sketch compiles down to about 6.5K on AVR.
-//
-// FastLED provides a few pre-configured color palettes, and makes it
-// extremely easy to make up your own color schemes with palettes.
-//
-// Some notes on the more abstract 'theory and practice' of
-// FastLED compact palettes are at the bottom of this file.
+// Don't touch these, internal color variation variables
+unsigned long setTime = COLOR_SHIFT;
+int shiftC = 0;
+int mulC = 2;
 
-
-
-CRGBPalette16 currentPalette;
-TBlendType    currentBlending;
-
-extern CRGBPalette16 myRedWhiteBluePalette;
-extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
-
-
-void setup() {
-    delay( 3000 ); // power-up safety delay
-    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-    FastLED.setBrightness(  BRIGHTNESS );
-    
-    currentPalette = RainbowColors_p;
-    currentBlending = LINEARBLEND;
-}
-
-
-void loop()
-{
-    ChangePalettePeriodically();
-    
-    static uint8_t startIndex = 0;
-    startIndex = startIndex + 1; /* motion speed */
-    
-    FillLEDsFromPaletteColors( startIndex);
-    
-    FastLED.show();
-    FastLED.delay(1000 / UPDATES_PER_SECOND);
-}
-
-void FillLEDsFromPaletteColors( uint8_t colorIndex)
-{
-    uint8_t brightness = 255;
-    
-    for( int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
-        colorIndex += 3;
-    }
-}
-
-
-// There are several different palettes of colors demonstrated here.
-//
-// FastLED provides several 'preset' palettes: RainbowColors_p, RainbowStripeColors_p,
-// OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
-//
-// Additionally, you can manually define your own color palettes, or you can write
-// code that creates color palettes on the fly.  All are shown here.
-
-void ChangePalettePeriodically()
-{
-    uint8_t secondHand = (millis() / 1000) % 60;
-    static uint8_t lastSecond = 99;
-    
-    if( lastSecond != secondHand) {
-        lastSecond = secondHand;
-        if( secondHand ==  0)  { currentPalette = RainbowColors_p;         currentBlending = LINEARBLEND; }
-        if( secondHand == 10)  { currentPalette = RainbowStripeColors_p;   currentBlending = NOBLEND;  }
-        if( secondHand == 15)  { currentPalette = RainbowStripeColors_p;   currentBlending = LINEARBLEND; }
-        if( secondHand == 20)  { SetupPurpleAndGreenPalette();             currentBlending = LINEARBLEND; }
-        if( secondHand == 25)  { SetupTotallyRandomPalette();              currentBlending = LINEARBLEND; }
-        if( secondHand == 30)  { SetupBlackAndWhiteStripedPalette();       currentBlending = NOBLEND; }
-        if( secondHand == 35)  { SetupBlackAndWhiteStripedPalette();       currentBlending = LINEARBLEND; }
-        if( secondHand == 40)  { currentPalette = CloudColors_p;           currentBlending = LINEARBLEND; }
-        if( secondHand == 45)  { currentPalette = PartyColors_p;           currentBlending = LINEARBLEND; }
-        if( secondHand == 50)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = NOBLEND;  }
-        if( secondHand == 55)  { currentPalette = myRedWhiteBluePalette_p; currentBlending = LINEARBLEND; }
-    }
-}
-
-// This function fills the palette with totally random colors.
-void SetupTotallyRandomPalette()
-{
-    for( int i = 0; i < 16; i++) {
-        currentPalette[i] = CHSV( random8(), 255, random8());
-    }
-}
-
-// This function sets up a palette of black and white stripes,
-// using code.  Since the palette is effectively an array of
-// sixteen CRGB colors, the various fill_* functions can be used
-// to set them up.
-void SetupBlackAndWhiteStripedPalette()
-{
-    // 'black out' all 16 palette entries...
-    fill_solid( currentPalette, 16, CRGB::Black);
-    // and set every fourth one to white.
-    currentPalette[0] = CRGB::White;
-    currentPalette[4] = CRGB::White;
-    currentPalette[8] = CRGB::White;
-    currentPalette[12] = CRGB::White;
-    
-}
-
-// This function sets up a palette of purple and green stripes.
-void SetupPurpleAndGreenPalette()
-{
-    CRGB purple = CHSV( HUE_PURPLE, 255, 255);
-    CRGB green  = CHSV( HUE_GREEN, 255, 255);
-    CRGB black  = CRGB::Black;
-    
-    currentPalette = CRGBPalette16(
-                                   green,  green,  black,  black,
-                                   purple, purple, black,  black,
-                                   green,  green,  black,  black,
-                                   purple, purple, black,  black );
-}
-
-
-// This example shows how to set up a static color palette
-// which is stored in PROGMEM (flash), which is almost always more
-// plentiful than RAM.  A static PROGMEM palette like this
-// takes up 64 bytes of flash.
-const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM =
-{
-    CRGB::Red,
-    CRGB::Gray, // 'white' is too bright compared to red and blue
-    CRGB::Blue,
-    CRGB::Black,
-    
-    CRGB::Red,
-    CRGB::Gray,
-    CRGB::Blue,
-    CRGB::Black,
-    
-    CRGB::Red,
-    CRGB::Red,
-    CRGB::Gray,
-    CRGB::Gray,
-    CRGB::Blue,
-    CRGB::Blue,
-    CRGB::Black,
-    CRGB::Black
+struct color {
+  int r;
+  int g;
+  int b;
 };
+typedef struct color Color;
 
+void setup() { 
+    Serial.begin(9600);
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+    pinMode(A0, INPUT);
+    pinMode(A4, INPUT);
 
+    for(int i = 0; i < NUM_LEDS ; i++) {
+      leds[i] = CRGB(0,0,0);
+    }
+    FastLED.show();
+}
 
-// Additional notes on FastLED compact palettes:
-//
-// Normally, in computer graphics, the palette (or "color lookup table")
-// has 256 entries, each containing a specific 24-bit RGB color.  You can then
-// index into the color palette using a simple 8-bit (one byte) value.
-// A 256-entry color palette takes up 768 bytes of RAM, which on Arduino
-// is quite possibly "too many" bytes.
-//
-// FastLED does offer traditional 256-element palettes, for setups that
-// can afford the 768-byte cost in RAM.
-//
-// However, FastLED also offers a compact alternative.  FastLED offers
-// palettes that store 16 distinct entries, but can be accessed AS IF
-// they actually have 256 entries; this is accomplished by interpolating
-// between the 16 explicit entries to create fifteen intermediate palette
-// entries between each pair.
-//
-// So for example, if you set the first two explicit entries of a compact 
-// palette to Green (0,255,0) and Blue (0,0,255), and then retrieved 
-// the first sixteen entries from the virtual palette (of 256), you'd get
-// Green, followed by a smooth gradient from green-to-blue, and then Blue.
+void loop() { 
+  unsigned long time = millis();
+
+  // Shift the color spectrum by 200 on set intervals (setTime)
+  if(time / (double)setTime >= 1) {
+    setTime = time + COLOR_SHIFT;
+    Serial.println(setTime);
+    shiftC += 200;
+    mulC++;
+    if(shiftC >= 600) {
+      shiftC = 0;
+    }
+    if(mulC > 3) {
+      mulC = 2;
+    }
+  }
+
+  // Shift all LEDs to the right by updateLEDS number each time
+  for(int i = NUM_LEDS - 1; i >= updateLEDS; i--) {
+    leds[i] = leds[i - updateLEDS];
+  }
+
+  // Get the pitch and brightness to compute the new color
+  int newPitch = (analogRead(PITCH_PIN)*2) + shiftC;
+  Color nc = pitchConv(newPitch, analogRead(BRIGHT_PIN));
+
+  // Set the left most updateLEDs with the new color
+  for(int i = 0; i < updateLEDS; i++) {
+    leds[i] = CRGB(nc.r, nc.g, nc.b);
+  }
+  FastLED.show();
+
+  //printColor(nc);
+  delay(1);
+}
+
+/**
+ * Converts the analog brightness reading into a percentage
+ * 100% brightness is 614.. about 3 volts based on frequency to voltage converter circuit
+ * The resulting percentage can simply be multiplied on the rgb values when setting our colors,
+ * for example black is (0,0,0) so when volume is off we get 0v and all colors are black (leds are off)
+ */
+double convBrightness(int b) {
+  double c = b / 614.0000;
+  if( c < 0.2 ) {
+    c = 0;
+  }
+  else if(c > 1) {
+    c = 1.00;
+  }
+  return c;
+}
+
+/**
+ * Creates a new color from pitch and brightness readings
+ * int p         analogRead(pitch) representing the voltage between 0 and 5 volts
+ * double b      analogRead(brightness) representing volume of music for LED brightness
+ * returns Color structure with rgb values, which appear synced to the music
+ */
+Color pitchConv(int p, int b) {
+  Color c;
+  double bright = convBrightness(b);
+
+  if(p < 40) {
+    setColor(&c, 255, 0, 0);
+  }
+  else if(p >= 40 && p <= 77) {
+    int b = (p - 40) * (255/37.0000);
+    setColor(&c, 255, 0, b);
+  }
+  else if(p > 77 && p <= 205) {
+    int r = 255 - ((p - 78) * 2);
+    setColor(&c, r, 0, 255);
+  }
+  else if(p >= 206 && p <= 238) {
+    int g = (p - 206) * (255/32.0000);
+    setColor(&c, 0, g, 255);
+  }
+  else if(p <= 239 && p <= 250) {
+    int r = (p - 239) * (255/11.0000);
+    setColor(&c, r, 255, 255);
+  }
+  else if(p >= 251 && p <= 270) {
+    setColor(&c, 255, 255, 255);
+  }
+  else if(p >= 271 && p <= 398) {
+    int rb = 255-((p-271)*2);
+    setColor(&c, rb, 255, rb);
+  }
+  else if(p >= 398 && p <= 653) {
+    setColor(&c, 0, 255-(p-398), (p-398));
+  }
+  else {
+    setColor(&c, 255, 0, 0);
+  }
+  setColor(&c, c.r * bright, c.g * bright, c.b * bright);
+  return c;
+}
+
+void setColor(Color *c, int r, int g, int b) {
+  c->r = r;
+  c->g = g;
+  c->b = b;
+}
+
+// Prints color structure data
+void printColor(Color c) {
+  Serial.print("( ");
+  Serial.print(c.r);
+  Serial.print(", ");
+  Serial.print(c.g);
+  Serial.print(", ");
+  Serial.print(c.b);
+  Serial.println(" )");
+}
