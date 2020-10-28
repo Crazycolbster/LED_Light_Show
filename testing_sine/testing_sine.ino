469edc293f1eb0cc1c7975afc1482ef7ba231ee6
Vetaz <47232579+Vetaz@users.noreply.github.com>
2020-10-28 13:03:38 -0600

    added differeing brightnesses and frequencies

--- testing_sine/testing_sine.ino
+++ testing_sine/testing_sine.ino
@@ -7,6 +7,13 @@ static float sineWave[32] = {0.00000,0.09755,0.19134,0.27779,0.35355,0.41573,0.4
 int sineWaveSize;
 static int sampleRate = 8000;
 
+static uint16_t frequencies[10] = {60, 90, 120, 240, 480, 960, 1250, 2500, 5000, 10000};
+static uint8_t currentFreqNum = 0;
+
+static uint8_t brightnesses[18] = {130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 210, 200, 190, 180, 170, 160, 150, 140};
+static uint8_t currentBrightnessNum = 0;
+
+//TODO: volume changes num leds on...
 // Sine wave: y = A sin (k (theta - b)) + c
 // A = Amplitude, k = periodicity factor, b = left/right shift, c = up/down shift
 
@@ -25,7 +32,7 @@ static int sampleRate = 8000;
 #define BRIGHTNESS  32 // It can be higher, but keep at 32 for now?
 #define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
-#define UPDATES_PER_SECOND 100 // how fast the LEDs change color/brightness per second.
+#define UPDATES_PER_SECOND 20 // how fast the LEDs change color/brightness per second.
 
 CRGB leds[NUM_LEDS];
 TBlendType    currentBlending;
@@ -36,8 +43,6 @@ void setup() {
   Serial.begin(9600);
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS); 
   FastLED.setBrightness(BRIGHTNESS);
-  currentPalette = RainbowColors_p;
-  currentBlending = LINEARBLEND;
 
   sineWaveSize = sizeof(sineWave) / sizeof(float);
 }
@@ -46,32 +51,39 @@ void FillLEDsFromPaletteColors(uint8_t hue, uint8_t brightness, uint8_t saturati
 {
   // Brightness from 0-255;
   // Saturation from 0-255;
+
+  // Turn on a fraction of the LEDs if birghtness is not turned all the way up
+  uint8_t adjNumLEDs = brightness / 255 * NUM_LEDS; 
   for(int i = 0; i < NUM_LEDS; i++) {
     leds[i] = CHSV(hue, saturation, brightness);
-    Serial.println(hue);
   }
+//  for(int i = adjNumLEDs; i < NUM_LEDS; i++) {
+//    leds[i] = CHSV(hue, saturation, 0);
+//  }
 }
 
 /* Get the pitch frequency */
 float getFreq() {
-  return 800; // 250 Hz
+  currentFreqNum++;
+  return frequencies[currentFreqNum % 11]; // Going through the frequencies each time this function is called.
 }
 
 /* Get the volume amplitude 
    returns a number from 0 to 5 */
-float getAmp(){
-  return 3;
+uint8_t getBright(){
+  currentBrightnessNum++;
+  return brightnesses[currentBrightnessNum % 19]; // Going through the brightnesses each time this function is called.
 }
 
 void loop() { 
   static uint8_t hue = 0;
-  static uint8_t brightness = 255;
   static uint8_t saturation = 255;
   
   static uint8_t startIndex = 0;
   startIndex = startIndex + 1; /* motion speed */
 
   float freq = getFreq();
+  uint8_t brightness = getBright();
 
   // Changes color according to sample amplitude's absolute value.
 //  if (startIndex >= sineWaveSize) {
